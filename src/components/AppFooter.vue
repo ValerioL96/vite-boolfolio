d0f33dc22c4196faadba1e93c08fea754682b4ba
ValerioL96 <v.lembo1396@gmail.com>
2024-09-10 13:15:24 +0200

    fixed footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,7 +6,49 @@ export default  {
 
     data(){
             return{
-    
+                navLinkNames: [
+                {
+                    label: "Homepage",
+                    name: "home"
+
+                },
+                {
+                    label: "Project",
+                    name: "blog"
+
+                },
+                {
+                    label: "About",
+                    name: "about"
+
+                },
+            ],
+            contactsList:[
+                {
+                    id:1,
+                    name:'3473443637',
+                    image:'src/assets/icon/smartphone (1).png',
+                },
+                {
+                    id:2,
+                    name:'v.lembo1396@gmail.com',
+                    image:'src/assets/icon/email.png',
+                },
+            ],
+            socialsList:[
+                {
+                    id:1,
+                    name:'valeriolembo',
+                    image:'src/assets/icon/linkedin.png',
+                    url: 'https://www.linkedin.com/in/valeriolembo/'
+                },
+                {
+                    id:2,
+                    name:'ValerioL96',
+                    image:'src/assets/icon/github.png',
+                    url: 'https://github.com/ValerioL96'
+                },
+            ]
             }
         },
 
@@ -15,40 +57,64 @@ export default  {
 </script>
 
 <template>
-    <ul class="nav  bg-danger d-flex justify-content-center">
-        <li class="nav-item">
-            <h6>
-                <a class="nav-link" aria-current="page" href="#">Link</a>
-            </h6>   
-        </li>
-        <li class="nav-item">
-            <h6>
-                <a class="nav-link" href="#">Link</a>
-            </h6>
-        </li>
-        <li class="nav-item">
-            <h6>
-                <a class="nav-link" href="#">Link</a>
-            </h6>
-        </li>
-        <li class="nav-item">
-            <h6>
-                <a class="nav-link" href="#">Link</a>
-            </h6>
-        </li>
-        <li class="nav-item">
-            <h6>
-                <a class="nav-link" href="#">Link</a>
-            </h6>
-        </li>
-        <li class="nav-item">
-            <h6>
-                <a class="nav-link" href="#">Link</a>
-            </h6>
-        </li>
-    </ul>
+    <nav class="navbar ">
+        <div class="container-fluid d-flex flex-row justify-content-around">
+            <section class="d-flex flex-column">
+                <h2 class="navbar-brand">Resource</h2>
+            
+                <ul class="navbar-nav">
+                    <li class="nav-item me-4 " v-for="navItem in navLinkNames">
+                        <router-link :to="{ name: navItem.name }" class="links">
+                            {{ navItem.label }}
+                        </router-link>
+                    </li>
+                </ul>
+            </section>
+
+            <section class="d-flex flex-column">
+                <h2 class="navbar-brand">Contacts</h2>
+            
+                <ul class="navbar-nav">
+                    <li v-for="social in contactsList" :key="social.id" class="d-flex flex-row">
+                        <img :src="social.image" alt="social-image" class="icon-image mx-2">
+                        <span>
+                            <a class="nav-link" aria-current="page" :href="social.url">
+                                <h6>
+                                    {{ social.name }}
+                                </h6>
+                            </a>
+                        </span>
+                    </li>
+                </ul>
+            </section>
+            
+            <section class="d-flex flex-column">
+                <h2 class="navbar-brand">Socials</h2>
+            
+                <ul class="navbar-nav">
+                    <li v-for="social in socialsList" :key="social.id" class="d-flex flex-row">
+                        <img :src="social.image" alt="social-image" class="icon-image mx-2">
+                        <span>
+                            <a class="nav-link" aria-current="page" :href="social.url">
+                                <h6>
+                                    {{ social.name }}
+                                </h6>
+                            </a>
+                        </span>
+                    </li>
+                </ul>
+            </section>
+        </div>
+    </nav>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+
+
+.icon-image{
+    
+    height: 30px;
+    width: 30px;
+}
 
 </style>
---
+++ src/pages/PageHomeJumboTron.vue
@@ -0,0 +1,50 @@
+<script>
+export default {
+    data() {
+        return {
+            carouselImagesList: [
+                {
+                    id: 1,
+                    name: 'Boolzapp',
+                    image: 'src/assets/img/Screenshot_6-9-2024_17587_.jpeg',
+                },
+                {
+                    id: 2,
+                    name: 'Discord',
+                    image: 'src/assets/img/Screenshot_6-9-2024_175638_.jpeg',
+                },
+                {
+                    id: 3,
+                    name: 'Boolfolio',
+                    image: 'src/assets/img/Screenshot_30-8-2024_114828_127.0.0.1.jpeg',
+                },
+            ],
+        };
+    },
+    methods: {},
+};
+</script>
+
+<template>
+    <!-- Aggiunto data-bs-interval per avere uno scorrimento automatico del carosello -->
+    <div id="carouselExample" class="carousel slide" data-bs-ride="carousel" data-bs-interval="3000">
+        <div class="carousel-inner">
+
+            <div 
+                v-for="(carouselImage, index) in carouselImagesList" 
+                :key="carouselImage.id" 
+                class="carousel-item"
+                :class="{ active: index === 0 }" 
+            >
+                <div class="text-center">
+                    <h5 class="mb-3">{{ carouselImage.name }}</h5>
+                    <img :src="carouselImage.image" alt="carousel-image" class="img-fluid w-50 border border-secondary rounded">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
